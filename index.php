0011172ea12309a41d9fbe5fe0498bbb9d77c4af
traderbook <contact@traderbook.net>
2018-11-27 12:13:15 +0100

    Ajout d'un construteur

--- index.php
+++ index.php
@@ -1,56 +1,79 @@
 <?php
 
 // Création de la class
-class Person {
+class Person
+{
     // Création des propriétés
     private $firstname;
     private $lastname;
     // Je ne peux pas accéder à la propriété privée password.
     private $password;
 
-    public function setFirstname($valueFirstName) {
+    public function __construct($valueFirstname, $valueLastname, $valuePassword)
+    {
+        $this->firstname = $valueFirstname;
+        $this->lastname = $valueLastname;
+        $this->password= $valuePassword;
+    }
+
+    public function setFirstname($valueFirstName)
+    {
         $this->firstname = $valueFirstName;
     }
 
-    public function getFirstname() {
+    public function getFirstname()
+    {
         return $this->firstname;
     }
 
-    public function setLastname($valueLastName) {
+    public function setLastname($valueLastName)
+    {
         $this->lastname = $valueLastName;
     }
 
-    public function getLastname() {
+    public function getLastname()
+    {
         return $this->lastname;
     }
 
-    public function setPassword($valuePassword) {
+    public function setPassword($valuePassword)
+    {
         $this->password = $valuePassword;
     }
 
-    public function getPassword() {
+    public function getPassword()
+    {
         return $this->password;
     }
 }
 
 // Création d'un objet de type Person
-$bibi = new Person();
+$bibi = new Person(
+    "Bibi",
+    "Bob",
+    "bibiestalamaison");
 
-$bibi->setFirstname("Bibi");
-$bibi->setLastname("Bob");
-$bibi->setPassword("bibiestalamaison");
+var_dump($bibi);
 
-var_dump($bibi->getFirstname());
-var_dump($bibi->getLastname());
-var_dump($bibi->getPassword());
-
-//$john = new Person();
+//$bibi->setFirstname("Bibi");
+//$bibi->setLastname("Bob");
+//$bibi->setPassword("bibiestalamaison");
 //
+//var_dump($bibi->getFirstname());
+//var_dump($bibi->getLastname());
+//var_dump($bibi->getPassword());
+
+$john = new Person(
+    "John",
+    "Doe",
+    "johnestalamaison"
+);
+
 //$john->setFirstname("John");
 //$john->setLastname("Doe");
 //$john->setPassword("johnestalamaison");
 //
-//var_dump($john);
+var_dump($john);
 
 
 
