dd4829e7b0f2fd3dcd15803413124ebbbc5c09da
traderbook <contact@traderbook.net>
2018-11-28 10:22:34 +0100

    Héritage de class

--- extends.php
+++ extends.php
@@ -1,8 +1,20 @@
 <?php
 
 class Mammal {
-    private $year;
-    private $sex;
+    private $year = 0;
+    private $sex = "NC";
+
+    public function getYear() {
+        return $this->year;
+    }
+
+    public function getSex() {
+        return $this->sex;
+    }
+
+    public function addYear() {
+        $this->year++;
+    }
 }
 
 class Person extends Mammal {
@@ -17,5 +29,14 @@ class Dog extends Mammal {
 $person = new Person();
 $dog = new Dog();
 
-var_dump($person);
-var_dump($dog);
+var_dump("Person");
+var_dump($person->getYear());
+$person->addYear();
+var_dump($person->getYear());
+
+var_dump("Dog");
+var_dump($dog->getYear());
+$dog->addYear();
+$dog->addYear();
+$dog->addYear();
+var_dump($dog->getYear());
