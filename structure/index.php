db754a88444eb4564e3cd2abbd0719efed233d9c
traderbook <contact@traderbook.net>
2018-11-28 13:57:01 +0100

    namepsapce

--- structure/index.php
+++ structure/index.php
@@ -3,12 +3,12 @@ require_once "libs/Animal.php";
 require_once "libs/Mammal.php";
 
 require_once "classes/Person.php";
-//require_once "classes/Dog.php";
+require_once "classes/Dog.php";
 
 use classes\Person as P;
 
 $peron = new P("john", "Doe", "black", "18", "male");
 var_dump($peron);
 
-//$dog = new Dog("black", 3, "female");
-//var_dump($dog);
+$dog = new Dog("black", 3, "female");
+var_dump($dog);
