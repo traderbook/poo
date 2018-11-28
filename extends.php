38ab39cfc7fe7a6a16db5c0b827cbe2c51435787
traderbook <contact@traderbook.net>
2018-11-28 11:19:16 +0100

    Héritage de class

--- extends.php
+++ extends.php
@@ -1,6 +1,6 @@
 <?php
 
-class Mammal {
+class Animal {
     private $year = 0;
     private $sex = "NC";
 
@@ -17,26 +17,48 @@ class Mammal {
     }
 }
 
+class Mammal extends Animal {
+    private $color;
+
+    public function __construct($pColor)
+    {
+        $this->color = $pColor;
+    }
+}
+
+class Oviparous extends Animal {
+    private $color;
+
+    public function __construct($pColor)
+    {
+        $this->color = $pColor;
+    }
+}
+
+class Gecko extends Oviparous {
+    private $name;
+}
+
 class Person extends Mammal {
     private $firstname;
     private $lastname;
+
+    public function __construct($pFirstname, $pLastname, $pColor)
+    {
+        parent::__construct($pColor);
+
+        $this->firstname = $pFirstname;
+        $this->lastname = $pLastname;
+    }
 }
 
 class Dog extends Mammal {
     private $name;
 }
 
-$person = new Person();
-$dog = new Dog();
+$person = new Person("john", "doe", "beige", 1, "male");
+$dog = new Dog("noir");
+$gecko = new Gecko("vert");
 
-var_dump("Person");
-var_dump($person->getYear());
-$person->addYear();
-var_dump($person->getYear());
-
-var_dump("Dog");
-var_dump($dog->getYear());
-$dog->addYear();
-$dog->addYear();
-$dog->addYear();
-var_dump($dog->getYear());
+var_dump($person);
+var_dump($gecko);
